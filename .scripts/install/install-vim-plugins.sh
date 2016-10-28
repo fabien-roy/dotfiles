36b0bdebc32df43953a4646941928a47bb4c165c
Fabien Roy <exilednarwal28@gmail.com>
2016-10-28 09:41:17 -0400

    Update install-vim-plugins.sh

--- .scripts/install/install-vim-plugins.sh
+++ .scripts/install/install-vim-plugins.sh
@@ -1,2 +1,6 @@
 # This file installs everything I need for vim to work
 
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
