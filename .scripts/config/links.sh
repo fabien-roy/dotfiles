36b26fd3627ff256881245db5da4bdd626972154
Fabien Roy <exilednarwal28@gmail.com>
2017-01-17 10:10:13 -0500

    Add i3 files

--- .scripts/config/links.sh
+++ .scripts/config/links.sh
@@ -5,3 +5,7 @@ ln ~/.dotfiles/.profile     ~/.profile
 ln ~/.dotfiles/.vimrc       ~/.vimrc
 ln ~/.dotfiles/.zsh_aliases ~/.zsh_aliases
 ln ~/.dotfiles/.zshrc       ~/.zshrc
+
+ln ~/.dotfiles/.i3          ~/.i3
+ln ~/.dotfiles/.i3blocks    ~/.i3blocks
+ln ~/.dotfiles/.i3-gaps     ~/.i3-gaps
