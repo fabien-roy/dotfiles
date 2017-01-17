27c96d2cbb493037fae710e7b2225ce9e0dce142
Fabien Roy <exilednarwal28@gmail.com>
2017-01-17 10:18:55 -0500

    Update linking script Need to work on i3blocks

--- .scripts/config/links.sh
+++ .scripts/config/links.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln ~/.dotfiles/.tmux.conf   ~/.tmux.conf
-ln ~/.dotfiles/.profile     ~/.profile
-ln ~/.dotfiles/.vimrc       ~/.vimrc
-ln ~/.dotfiles/.zsh_aliases ~/.zsh_aliases
-ln ~/.dotfiles/.zshrc       ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf   ~/.tmux.conf
+ln -s ~/.dotfiles/.profile     ~/.profile
+ln -s ~/.dotfiles/.vimrc       ~/.vimrc
+ln -s ~/.dotfiles/.zsh_aliases ~/.zsh_aliases
+ln -s ~/.dotfiles/.zshrc       ~/.zshrc
 
-ln ~/.dotfiles/.i3          ~/.i3
-ln ~/.dotfiles/.i3blocks    ~/.i3blocks
-ln ~/.dotfiles/.i3-gaps     ~/.i3-gaps
+ln -s ~/.dotfiles/.i3          ~/.i3
+ln -s ~/.dotfiles/.i3blocks    ~/.i3blocks
+ln -s ~/.dotfiles/.i3-gaps     ~/.i3-gaps
