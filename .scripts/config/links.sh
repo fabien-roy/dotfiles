8fee3c96e43d6f9542a9e269855b40180915b0c7
Fabien Roy <exilednarwal28@gmail.com>
2017-01-17 09:50:04 -0500

    Add linking script

---
+++ .scripts/config/links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln ~/.dotfiles/.tmux.conf   ~/.tmux.conf
+ln ~/.dotfiles/.profile     ~/.profile
+ln ~/.dotfiles/.vimrc       ~/.vimrc
+ln ~/.dotfiles/.zsh_aliases ~/.zsh_aliases
+ln ~/.dotfiles/.zshrc       ~/.zshrc
