c96ef302271c4ee2309eb96cf14f41695a95b2b6
Fabien Roy <fabienroy28@gmail.com>
2018-06-05 20:07:26 -0400

    Cleanup of i3 things

--- .i3/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Thanks to /u/xrmb123 from https://www.reddit.com/r/unixporn/comments/5m15wj/gnome_pixellated_lock_screen/
-scrot /home/$(whoami)/screen.png
-convert /home/$(whoami)/screen.png -scale 10% -scale 1000% /home/$(whoami)/screen.png
-[[ -f $1 ]] && convert /home/$(whoami)/screen.png $1 -gravity center -composite -matte /home/$(whoami)/screen.png
-sleep 1
-xscreensaver-command -l
--- .i3/switch-sound-input.sh
+++
@@ -1,1 +0,0 @@
-switch-sound.sh
